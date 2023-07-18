e0b65d4d4c3036ac31cc10d416ca0af0a64d3aa1
Tim Montague <tim.john.montague@gmail.com>
2023-07-18 05:01:28 -0400

    Fixed problem, With user asked to continue, Valid entry

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ do
     bool valid = false;
     do
     {
+        //Getting input from the user 
+        Console.Clear();
         Console.Write("Learn your squares and cubes!\n" +
         "Enter an interger: ");
         userInput = int.Parse(Console.ReadLine());
@@ -15,6 +17,7 @@ do
             Console.WriteLine("Sorry no negative numbers or 0, please try again\n ");
             valid = false;
         }
+        //It is important not to go above 1290 because when cubed it will cause the integer to overflow 
         else if (userInput > 1290)
         {
             Console.WriteLine("number is too high, please try again\n ");
@@ -25,17 +28,27 @@ do
             valid = true;
         }
     } while (!valid);
-
+    //Displays the results of each method 
     Console.WriteLine("{0,-10}{1,-10}{2,-13}", "Numbers", "Squared", "Cubed");
     for (int i = 1; i <= userInput; i++)
     {
         Console.WriteLine(String.Format("{0,-10}{1,-10}{2,-13}", (i), Squared(i),Cubed(i)));
     }
     Console.WriteLine();
-
-    Console.Write("Continue? (y/n):");
-    anser = Console.ReadLine().ToLower().Trim();
-
+    //Ask the user whether he or she wishes to continue 
+    while (true)  
+    {
+        Console.Write("Continue? (y/n):");
+        anser = Console.ReadLine().ToLower().Trim();
+        if (anser=="y"||anser=="n")
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("\nInvalid entry please try again.");
+        }
+    }
 
 } while (anser == "y");
 
